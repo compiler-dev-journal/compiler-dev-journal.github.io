5a260baaf756625af746705084cf75b8db436b58
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2019-01-22 12:18:19 +0000

    Proper domain name

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_TITLE = "Языки и компиляторы"  # (translatable)
 BLOG_AUTHOR = BLOG_TITLE  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://plcomp.github.io/"
+SITE_URL = "https://plcomp.org/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://compiler-dev-journal.github.io/"
